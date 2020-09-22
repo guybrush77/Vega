018c1dc0bf9a23d54c36d045807b7b251bddce78
Slobodan Pavlic <slobodan.pavlic@gmail.com>
2020-09-22 10:42:49 -0400

    Added glfw (window & input library) as external project

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
+#include <GLFW/glfw3.h>
 #include <spdlog/spdlog.h>
 
 int main()
 {
-    spdlog::info("Hello World");
+    int success = glfwInit();
+    if (!success)
+    {
+        throw std::runtime_error("glfwInit failed");
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!window)
+    {
+        throw std::runtime_error("glfwCreateWindow failed");
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwWaitEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
