f7ef20162818090811b095e835f7783474ea429e
Slobodan Pavlic <slobodan.pavlic@gmail.com>
2020-10-02 17:51:38 -0400

    Added more helper functions to etna Device

--- src/etna/device.cpp
+++ src/etna/device.cpp
@@ -10,8 +10,15 @@
 
 namespace {
 
-struct QueueFamilies {
-    std::uint32_t graphics = 0;
+struct QueueInfo final {
+    uint32_t family_index = 0;
+    uint32_t queue_count  = 0;
+};
+
+struct QueueIndices final {
+    std::optional<QueueInfo> graphics;
+    std::optional<QueueInfo> compute;
+    std::optional<QueueInfo> transfer;
 };
 
 static vk::PhysicalDevice GetGpu(vk::Instance instance)
@@ -22,35 +29,46 @@ static vk::PhysicalDevice GetGpu(vk::Instance instance)
     return gpus[0];
 }
 
-static QueueFamilies GetQueueFamilies(vk::PhysicalDevice gpu)
+static QueueIndices GetQueueIndices(vk::PhysicalDevice gpu)
 {
-    std::optional<std::uint32_t> graphics_index;
+    QueueIndices queue_indices;
 
     auto available_families = gpu.getQueueFamilyProperties();
 
     for (std::size_t i = 0; i != available_families.size(); ++i) {
-        if (false == graphics_index.has_value()) {
+        uint32_t family_index = narrow_cast<uint32_t>(i);
+        uint32_t queue_count  = available_families[i].queueCount;
+        if (false == queue_indices.graphics.has_value()) {
             if (available_families[i].queueFlags & vk::QueueFlagBits::eGraphics) {
-                graphics_index = narrow_cast<std::uint32_t>(i);
+                queue_indices.graphics = { family_index, queue_count };
+            }
+        }
+        if (false == queue_indices.compute.has_value()) {
+            if (available_families[i].queueFlags & vk::QueueFlagBits::eCompute) {
+                queue_indices.compute = { family_index, queue_count };
+            }
+        }
+        if (false == queue_indices.transfer.has_value()) {
+            if (available_families[i].queueFlags & vk::QueueFlagBits::eTransfer) {
+                queue_indices.transfer = { family_index, queue_count };
             }
         }
     }
-    if (false == graphics_index.has_value()) {
-        throw_runtime_error("GPU does not support Vulkan graphics");
-    }
 
-    return { graphics_index.value() };
+    return queue_indices;
 }
 
-static auto GetDeviceQueueCreateInfos(vk::PhysicalDevice gpu)
+static auto GetDeviceQueueCreateInfos(const QueueIndices& queue_indices)
 {
     static float queue_priority = 1.0f;
 
-    auto queue_families = GetQueueFamilies(gpu);
+    if (false == queue_indices.graphics.has_value()) {
+        throw_runtime_error("GPU does not support Vulkan graphics");
+    }
 
     vk::DeviceQueueCreateInfo graphics_create_info;
     {
-        graphics_create_info.queueFamilyIndex = queue_families.graphics;
+        graphics_create_info.queueFamilyIndex = queue_indices.graphics->family_index;
         graphics_create_info.queueCount       = 1;
         graphics_create_info.pQueuePriorities = &queue_priority;
     }
@@ -59,14 +77,73 @@ static auto GetDeviceQueueCreateInfos(vk::PhysicalDevice gpu)
     return create_infos;
 }
 
+struct DeviceInfo final {
+    vk::Instance       instance{};
+    vk::PhysicalDevice gpu{};
+    QueueIndices       queue_indices{};
+};
+
+class Context final {
+  public:
+    static Context& Instance()
+    {
+        static Context context;
+        return context;
+    }
+
+    void Set(VkDevice device, DeviceInfo device_info) noexcept
+    {
+        std::lock_guard<std::mutex> lock(m_lock);
+        m_info_map[device] = std::move(device_info);
+    }
+
+    DeviceInfo Get(VkDevice device) noexcept
+    {
+        std::lock_guard<std::mutex> lock(m_lock);
+        return m_info_map[device];
+    }
+
+  private:
+    struct Hash {
+        std::size_t operator()(VkDevice device) { return reinterpret_cast<std::size_t>(device); }
+    };
+
+    using info_map_t = std::unordered_map<VkDevice, DeviceInfo>;
+
+    std::mutex m_lock;
+    info_map_t m_info_map;
+};
+
 } // namespace
 
 namespace etna {
 
+auto GetGraphicsQueueFamilyIndex(vk::Device device) -> uint32_t
+{
+    return Context::Instance().Get(device).queue_indices.graphics->family_index;
+}
+
+auto AllocateUniqueCommandBuffer(vk::Device             device,
+                                 vk::CommandPool        command_pool,
+                                 vk::CommandBufferLevel command_buffer_level) -> vk::UniqueCommandBuffer
+{
+    vk::CommandBufferAllocateInfo allocate_info;
+    {
+        allocate_info.commandPool        = command_pool;
+        allocate_info.level              = command_buffer_level;
+        allocate_info.commandBufferCount = 1;
+    }
+
+    auto command_buffer = std::move(device.allocateCommandBuffersUnique(allocate_info).front());
+
+    return command_buffer;
+}
+
 vk::UniqueDevice CreateUniqueDevice(vk::Instance instance)
 {
     auto gpu                = GetGpu(instance);
-    auto queue_create_infos = GetDeviceQueueCreateInfos(gpu);
+    auto queue_indices      = GetQueueIndices(gpu);
+    auto queue_create_infos = GetDeviceQueueCreateInfos(queue_indices);
 
     vk::DeviceCreateInfo create_info;
     {
@@ -81,9 +158,18 @@ vk::UniqueDevice CreateUniqueDevice(vk::Instance instance)
 
     auto device = gpu.createDeviceUnique(create_info);
 
+    Context::Instance().Set(device.get(), { instance, gpu, std::move(queue_indices) });
+
     spdlog::info(COMPONENT "Created VkDevice {}", device.get());
 
     return device;
+}
+
+etna::UniqueAllocator CreateUniqueAllocator(vk::Device device)
+{
+    DeviceInfo device_info = Context::Instance().Get(device);
+
+    return etna::UniqueAllocator(device_info.instance, device_info.gpu, device);
 }
 
 } // namespace etna
--- src/etna/etna/device.hpp
+++ src/etna/etna/device.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
+#include "allocator.hpp"
+
 #include <vulkan/vulkan.hpp>
 
 namespace etna {
 
+auto GetGraphicsQueueFamilyIndex(vk::Device device) -> uint32_t;
+
+auto AllocateUniqueCommandBuffer(vk::Device             device,
+                                 vk::CommandPool        command_pool,
+                                 vk::CommandBufferLevel command_buffer_level = vk::CommandBufferLevel::ePrimary)
+    -> vk::UniqueCommandBuffer;
+
 auto CreateUniqueDevice(vk::Instance instance) -> vk::UniqueDevice;
+
+auto CreateUniqueAllocator(vk::Device device) -> etna::UniqueAllocator;
 
 } // namespace etna
